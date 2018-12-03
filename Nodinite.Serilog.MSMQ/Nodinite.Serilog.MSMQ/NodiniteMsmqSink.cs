533146df948664a3029f3d3f2fff5a92d5d83fa4
Robert Mayer <robert.mayer@integrationsoftware.se>
2018-12-03 16:11:56 +0100

    Initial sceleton for msmq logging

---
+++ Nodinite.Serilog.MSMQ/Nodinite.Serilog.MSMQ/NodiniteMsmqSink.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Nodinite.Serilog.Models;
+using Serilog.Core;
+using Serilog.Events;
+
+namespace Nodinite.Serilog.MSMQ
+{
+    public class NodiniteMsmqSink : ILogEventSink
+    {
+        private readonly IFormatProvider _formatProvider;
+        private readonly string _apiUrl;
+        private readonly NodiniteLogEventSettings _settings;
+
+        public NodiniteMsmqSink(string apiUrl, NodiniteLogEventSettings settings, IFormatProvider formatProvider)
+        {
+            _apiUrl = apiUrl;
+            _settings = settings;
+            _formatProvider = formatProvider;
+
+            // validate settings
+            if (!_settings.LogAgentValueId.HasValue)
+                throw new ArgumentNullException("LogAgentValueId must not be null");
+        }
+
+        public void Emit(LogEvent logEvent)
+        {
+            var message = logEvent.RenderMessage(_formatProvider);
+
+            var nEvent = new NodiniteLogEvent(message, logEvent, _settings);
+
+            LogMessage(nEvent);
+        }
+
+        public void LogMessage(NodiniteLogEvent logEvent)
+        {
+            // post message to msmq
+        }
+    }
+}
---
+++ Nodinite.Serilog.MSMQ/Nodinite.Serilog.MSMQ/NodiniteMsmqSinkExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Nodinite.Serilog.Models;
+using Serilog;
+using Serilog.Configuration;
+using Serilog.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nodinite.Serilog.MSMQ
+{
+    public static class NodiniteMsmqSinkExtensions
+    {
+        public static LoggerConfiguration NodiniteApiSink(
+                  this LoggerSinkConfiguration loggerConfiguration,
+                  string NodiniteApiUrl,
+                  NodiniteLogEventSettings Settings,
+                  IFormatProvider formatProvider = null,
+                  LogEventLevel restrictedToMinimumLevel = LevelAlias.Minimum)
+        {
+            if (loggerConfiguration == null)
+                throw new ArgumentNullException("loggerConfiguration");
+
+            return loggerConfiguration.Sink(new NodiniteApiSink(NodiniteApiUrl, Settings, formatProvider), restrictedToMinimumLevel);
+        }
+    }
+}
